f9cc505dc1b53ac338d96cd8ba85674ec3abb9c1
hnakashima96 <71496553+hnakashima96@users.noreply.github.com>
2022-04-11 20:29:19 +0100

    Sudoku attempt
    File from simulate annealing example from youtube 
    https://www.youtube.com/watch?v=FyyVbuLZav8

---
+++ Sudoku/data_sudoku.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+startingSudoku = """
+                    024007000
+                    600000000
+                    003680415
+                    431005000
+                    500000032
+                    790000060
+                    209710800
+                    040093000
+                    310004750
+                """
+
+sudoku = np.array([[int(i) for i in line] for line in startingSudoku.split()])
