a80b8089a300987d3d8fc2e4790d9090768e7a02
hnakashima96 <71496553+hnakashima96@users.noreply.github.com>
2022-05-23 21:56:30 +0100

    final

--- bancodados.py
+++ bancodados.py
@@ -13,14 +13,14 @@ def create_table_perform():
     )
 
     cursor = conn.cursor()
-    sql = "DROP TABLE IF EXISTS PERFORM; CREATE TABLE PERFORM (COMBINATION VARCHAR(25), TEST_NUMBER VARCHAR(25),TIME VARCHAR(25), POP_SIZE VARCHAR(25), SELECTION VARCHAR(25), CROSSOVER VARCHAR(25), CROSSOVER_PERCENT VARCHAR(25), MUT_PERCENT VARCHAR(25))"
+    sql = "DROP TABLE IF EXISTS PERFORM; CREATE TABLE PERFORM (COMBINATION VARCHAR(25), TEST_NUMBER VARCHAR(25),TIME VARCHAR(25), POP_SIZE VARCHAR(25), SELECTION VARCHAR(25), CROSSOVER VARCHAR(25), CROSSOVER_PERCENT VARCHAR(25), MUT_PERCENT VARCHAR(25), ELITISM_PERCENT VARCHAR(25))"
 
     cursor.execute(sql)
     print('Table create sucessfully')
     conn.commit()
     conn.close()
 
-def insere_perform(selection, co_option, test_number, time, pop_size, crossover_percent, mut1, mut2, mut_percent):
+def insere_perform(n_combinacao, selection, co_option, test_number, time, pop_size, crossover_percent, mut_percent, elitism_percent):
     conn = psycopg2.connect(
         database='CIFO', user='postgres', password='hiromi',
         host='localhost', port='5432'
@@ -28,26 +28,25 @@ def insere_perform(selection, co_option, test_number, time, pop_size, crossover_
 
     conn.autocommit = True
 
-    n_combination = selection.__name__+'_'+co_option.__name__
+    n_combination = selection.__name__+'_'+co_option.__name__+'_'+str(n_combinacao)
     n_selection = str(selection.__name__)
     n_co_option = str(co_option.__name__)
-    # n_mut1 = str(mut1)
-    # n_mut2 = str(mut2)
+
 
 
     cursor = conn.cursor()
 
-    cursor.execute("INSERT INTO PERFORM (COMBINATION, TEST_NUMBER, TIME, POP_SIZE, SELECTION, CROSSOVER, CROSSOVER_PERCENT, MUT_PERCENT) VALUES('"+n_combination+"',"+str(test_number)+","+str(time)+","+str(pop_size)+",'"+n_selection+"','"+n_co_option+"',"+str(crossover_percent)+","+str(mut_percent)+")")
+    cursor.execute("INSERT INTO PERFORM (COMBINATION, TEST_NUMBER, TIME, POP_SIZE, SELECTION, CROSSOVER, CROSSOVER_PERCENT, MUT_PERCENT, ELITISM_PERCENT) VALUES('"+n_combination+"',"+str(test_number)+","+str(time)+","+str(pop_size)+",'"+n_selection+"','"+n_co_option+"',"+str(crossover_percent)+","+str(mut_percent)+","+str(elitism_percent)+")")
 
     conn.commit()
     conn.close()
 
-def create_table_analysis(selection, co_option):
+def create_table_analysis(n_combinacao,selection, co_option):
     conn = psycopg2.connect(
         database = 'CIFO', user='postgres', password = 'hiromi',
         host = 'localhost', port = '5432'
     )
-    nome = selection.__name__+"_"+ co_option.__name__
+    nome = selection.__name__+'_'+co_option.__name__+'_'+str(n_combinacao)
 
     cursor = conn.cursor()
 
@@ -60,13 +59,13 @@ def create_table_analysis(selection, co_option):
     conn.commit()
     conn.close()
 
-def insere_teste (test_number,selection, co_option,count, fitness, diversidade):
+def insere_teste (test_number,n_combinacao,selection, co_option,count, fitness, diversidade):
     conn = psycopg2.connect(
         database='CIFO', user='postgres', password='hiromi',
         host='localhost', port='5432'
     )
 
-    nome = selection.__name__ + "_" + co_option.__name__
+    nome = selection.__name__+'_'+co_option.__name__+'_'+str(n_combinacao)
 
     conn.autocommit = True
 
--- charles/selection.py
+++ charles/selection.py
@@ -1,94 +1,89 @@
-from random import uniform,choice,sample
+from random import uniform, choices, sample
 from operator import attrgetter
-import numpy as np
 
 from Sudoku.functions import sharring_coef
 
+
 ### CREATE A ROULETTE WHEEL
 def roulette(population):
+    '''
+        The roulette wheel for the minization problem considers the sum of inverse fitness of
+        all each individual and each interaction is added the inverse fitness to the position
+        variable. With this configuration, higher values of fitness are smaller than lower ones,
+        thus the proportion of it in the wheel and in the probabilities.
 
-    sum_all_fitness = sum([population.individuals[ind].fitness for ind in range(len(population))])
-    
-    individual_prob = [population.individuals[ind].fitness/sum_all_fitness for ind in range(len(population))]
+    '''
+
+    position = 0
 
     if population.optim == 'min':
-        problem = 1 - np.array(individual_prob)
+        sum_all_fitness = sum([1 / ind.fitness for ind in population])
+        spin = uniform(0, sum_all_fitness)
+        for ind in population:
+            position += (1 / ind.fitness)
+            if position > spin:
+                return ind
 
     elif population.optim == 'max':
-        problem = np.array(individual_prob)
-
-    problem /= problem.sum()
-
-    choice = np.random.choice(population, p=problem)
-
-    return choice
+        sum_all_fitness = sum([ind.fitness for ind in population])
+        spin = uniform(0, sum_all_fitness)
+        for ind in population:
+            position += ind.fitness
+            if position > spin:
+                return ind
 
 
 ### CREATE RANKING SELECTION
 def rank(population):
+    '''
+        The rank selection considered the probability of select an individual based in his position in the
+        rank of fitness. The rank is just a sorting of the individuals by fitness: higher the position of
+        the individual in the list, higher the probability.
+        The probability is obtain by the formula:
+            individual index / AP(n)
+        Where AP(n) is the sum of all the index we obtain by the gaussing arithmetic progression formula:
+            n * (n+1) / 2    , n = number of elements or the higher index
 
-    if population.optim == 'min':
-        sort_all_fitness = [population.individuals[ind].fitness for ind in range(len(population))]
-        sort_all_fitness.sort(reverse=True)
-    elif population.optim == 'max':
-        sort_all_fitness = [population.individuals[ind].fitness for ind in range(len(population))]
-        sort_all_fitness.sort(reverse=False)
+    '''
+    # rank the population by sorting individuals bases on their fitness
+    rank = sorted(population, key=attrgetter("fitness"), reverse=population.optim == 'min')
 
-    individual_prob = np.array([ind/len(population) for ind in range(len(population))])
+    # get sum of index's
+    max_index = len(rank)
+    index_sum = (max_index * (max_index + 1)) / 2
 
-    individual_prob /= individual_prob.sum()
+    # creat a list with the probability for each individual be selected
+    individual_prob = [i / index_sum for i in range(1, max_index + 1)]
 
-    choice = np.random.choice(population, p=individual_prob)
-
-    return choice
+    return choices(rank, weights=individual_prob, k=1)
 
 
-def tournament(population, size=5):
+def tournament(population, size=5, sharing_cal=False):
     """Tournament selection implementation.
-
     Args:
         population (Population): The population we want to select from.
         size (int): Size of the tournament.
-
     Returns:
         Individual: Best individual in the tournament.
     """
 
-    # Select individuals based on tournament size
-    tournament = sample(population.individuals,size) 
-    # Check if the problem is max or min
+    if sharing_cal:
+        # Select individuals based on tournament size
+        tournament_indexs = sample(range(len(population)), size)
+
+        for i in tournament_indexs:
+            population[i].sharing_fitness = population[i].fitness / sharring_coef(i, population)
+
+        tournament = [population[i] for i in tournament_indexs]
+
+    else:
+        # Select individuals based on tournament size
+        tournament = sample(population.individuals, size)
+
+        # Check if the problem is max or min
     if population.optim == 'max':
         return max(tournament, key=attrgetter("fitness"))
     elif population.optim == 'min':
         return min(tournament, key=attrgetter("fitness"))
-    else:
-        raise Exception("No optimization specified (min or max).")
-
-
-
-def tournament2(population, size=5):
-    """Tournament selection implementation.
-
-    Args:
-        population (Population): The population we want to select from.
-        size (int): Size of the tournament.
-
-    Returns:
-        Individual: Best individual in the tournament.
-    """
- 
-    # Select individuals based on tournament size
-    tournament_indexs = sample(range(len(population)),size) 
-
-    for i in tournament_indexs:
-        population[i].sharing_fitness = population[i].fitness / sharring_coef(i,population)
-
-    tournament = [population[i] for i in tournament_indexs] 
-    
-    # Check if the problem is max or min
-    if population.optim == 'max':
-        return max(tournament, key=attrgetter("sharing_fitness"))
-    elif population.optim == 'min':
-        return min(tournament, key=attrgetter("sharing_fitness"))
     else:
         raise Exception("No optimization specified (min or max).")
